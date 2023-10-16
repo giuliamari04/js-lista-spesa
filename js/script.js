b8c43feaadda3bfd412a4c73191a02b3dbccc97d
giuliamari04 <giuliamari04@gmail.com>
2023-10-16 17:47:27 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -1,34 +1,46 @@
-const spesa=[];
-let input;
+let spesa=[];
+let input ='';
 const resultEl = document.querySelector(".alert");
 const listaSpesa = document.querySelector(".listaSpesa");
 
 
 while(input !=='stop'){
     input = prompt("Inserisci prodotti che vuoi acquistare:");
-    if(input === 'stop'){
-        break;
+    input = input.trim();
+
+    if(input !== 'stop' && input!==''){
+     spesa.push(input);  
     }
-    spesa.push(input);  
-    
+ 
 }
 
-if (spesa.length > 0) {
-    // resultEl.innerHTML = `
-    //  ecco la tua lista della spesa:
-    //  </br>
-    //  ${spesa}.
-    // `;
 
-    let listaHtml = "ecoo la tua lista : </br> <ul>";
-    for (let i = 0; i < spesa.length; i++) {
-        listaHtml += `<li>${spesa[i]}</li>`;
-    }
-    listaHtml += "</ul>";
-    listaSpesa.innerHTML = listaHtml;
+for(i=0;i<spesa.length;i++){
+    const listItemEl = document.createElement("li");
+    listItemEl.innerHTML= spesa[i];
+    //listItemEl.innerHTML='aggiungi'+list[i];
+    listItemEl.innerHTML = ` ${spesa[i]}`;
+    listaSpesa.appendChild(listItemEl);
+}
 
-    } else {
-        resultEl.innerHTML = ` nessun prodotto inserito`;
-    }
+
+
+// if (spesa.length > 0) {
+//     // resultEl.innerHTML = `
+//     //  ecco la tua lista della spesa:
+//     //  </br>
+//     //  ${spesa}.
+//     // `;
+
+//     let listaHtml = "ecoo la tua lista : </br> <ul>";
+//     for (let i = 0; i < spesa.length; i++) {
+//         listaHtml += `<li>${spesa[i]}</li>`;
+//     }
+//     listaHtml += "</ul>";
+//     listaSpesa.innerHTML = listaHtml;
+
+//     } else {
+//         resultEl.innerHTML = ` nessun prodotto inserito`;
+//     }
 
 console.log(spesa);
